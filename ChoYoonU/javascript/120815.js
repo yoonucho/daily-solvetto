7a0f11339bb099b93a6dd7e3c91e3073eba0772e
yoonucho <tomorrowcho@gmail.com>
2024-12-04 16:24:48 +0900

    프로그래머스 120815.피자 나눠 먹기(2) - 오타 수정

--- ChoYoonU/javascript/120815.js
+++ ChoYoonU/javascript/120815.js
@@ -1,4 +1,4 @@
-/* 프로그래머스 120814.피자 나눠 먹기(2)(https://school.programmers.co.kr/learn/courses/30/lessons/120815) */
+/* 프로그래머스 120815.피자 나눠 먹기(2)(https://school.programmers.co.kr/learn/courses/30/lessons/120815) */
 
 function solution(n) {
 	let pizzaPiece = 6;
