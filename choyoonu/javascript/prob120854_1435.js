73e7c57da96811391cf73e069b341b27bb763cbf
yoonucho <tomorrowcho@gmail.com>
2025-01-08 14:35:28 +0900

    Add solution for prob120854_1435
    Co-authored-by: yoonucho <tomorrowcho@gmail.com>

---
+++ choyoonu/javascript/prob120854_1435.js
@@ -0,0 +1,7 @@
+function solution(strlist) {
+    var answer = [];
+    answer = strlist.map((v)=> v.length);
+    return answer;
+}
+
+// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return하도록 solution 함수를 완성해주세요.
