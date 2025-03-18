3c0275115cec640dc469f6348f839a5169331672
yoonucho <tomorrowcho@gmail.com>
2025-03-18 15:48:41 +0900

    Add solution for prob11382_1548
    Co-authored-by: yoonucho <tomorrowcho@gmail.com>

---
+++ choyoonu/javascript/prob11382_1548.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let A = Number(input[0]);
+let B = Number(input[1]);
+let C = Number(input[2]);
+console.log(A+B+C);
